e7b1cef51ca011074609cf2b1dd334c0acaa4f35
Alex <alex.occhipinti@ausnetservices.com.au>
2024-08-08 14:40:12 +1000

    remove junk

--- Program.cs
+++ Program.cs
@@ -82,8 +82,3 @@ app.MapGet("/ping", () =>
 
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
