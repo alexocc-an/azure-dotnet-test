0218fc3339a2925ec50e2fb4d3c82a2f623377c5
Alex <alex.occhipinti@ausnetservices.com.au>
2024-08-09 16:58:27 +1000

    Add internal cache endpoint

--- Program.cs
+++ Program.cs
@@ -34,6 +34,56 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+List<Incident> incidentCache = [];
+DateTime lastCached = new();
+
+app.MapGet("/test_internal_cache", async () =>
+{
+    Console.WriteLine("Running read test with internal cache...");
+    if (DateTime.Now - lastCached >= new TimeSpan(0, 1, 0))
+    {
+        Database db = client.GetDatabase("test");
+        Container container = db.GetContainer("test");
+
+        using FeedIterator<Incident> feed = container.GetItemQueryIterator<Incident>(
+            queryText: "SELECT * FROM incidents i"
+        );
+
+        List<Incident> incidents = [];
+        List<string> queryLog = [];
+        int numQueries = 0;
+
+        // Return all paginated results
+        while (feed.HasMoreResults)
+        {
+
+            FeedResponse<Incident> response = await feed.ReadNextAsync();
+            queryLog.Add($"Got {response.Count} incidents from query number: {++numQueries}");
+
+            // Iterate query results
+            foreach (Incident i in response)
+            {
+                incidents.Add(i);
+            }
+        }
+
+        lastCached = DateTime.Now;
+        incidentCache = incidents;
+
+        queryLog.Add($"Fetched {incidents.Count} incidents in {numQueries} queries from Cosmos\n");
+
+        queryLog.Add(incidents.Count == 0 ? "no incident data" : incidents[0].ToString());
+
+        queryLog.ForEach(Console.WriteLine);
+        return string.Join('\n', queryLog);
+    }
+    var incidentString = incidentCache.Count == 0 ? "no incident data" : incidentCache[0].ToString();
+    return $"Using cached response from: {lastCached}\n\n{incidentCache.Count} incidents retrieved from internal cache.\n\n{incidentString}";
+})
+.WithName("TestInternalCache")
+.WithOpenApi();
+
+
 
 app.MapGet("/test", async () =>
 {
